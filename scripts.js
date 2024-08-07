b01c3457f45947accdec8935dce72be271b20a2d
kkimiiii <23015073@myrp.edu.sg>
2024-08-07 11:49:13 +0800

    third commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+//I declare that this code was written by me.
+//I will not coppy or allow others to copy my work.
+//I understand that copying code is considered as plagirsm.
+
+//Student Name: Hanan Hakimi
+//Student ID: 23015073
+//class: C369-3D-W66N-A-GA
+//Date/Time last modified: 2021/08/20 11:00
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const loggedIn = urlParams.get('loggedIn') === 'true';
--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+//I declare that this code was written by me.
+//I will not coppy or allow others to copy my work.
+//I understand that copying code is considered as plagirsm.
+
+//Student Name: Hanan Hakimi
+//Student ID: 23015073
+//class: C369-3D-W66N-A-GA
+//Date/Time last modified: 2021/08/20 11:00
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
